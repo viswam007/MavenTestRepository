a71b6d69c5761f6193badb2b3c2087ba3e119dae
viswam007 <viswam.i@gmail.com>
2020-10-06 21:08:37 -0400

    commiting as part of practice

---
+++ HelloWorld_Viswa/src/test/java/HelloWorld_Viswa/TestHelloWorld.java
@@ -0,0 +1,12 @@
+package HelloWorld_Viswa;
+
+import org.testng.annotations.Test;
+
+public class TestHelloWorld {
+	
+	@Test	
+	public void TestHelloWorld() {
+		System.out.println("....HelloWorld 2..");
+}
+
+}
